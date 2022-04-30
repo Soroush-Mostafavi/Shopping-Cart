acec78b5062c0a0d0e87b9419e229ecd27cb63b8
Soroush-Mostafavi <soroush.mr1994@gmail.com>
2022-04-30 13:29:51 +0430

    add part2&3

---
+++ src/dataautmn.js
@@ -0,0 +1,86 @@
+////////////////////////////////small///////////////////
+import image15 from "./assets/atumn/atumn-smalll.jpeg";
+import image16 from "./assets/atumn/atumn-small3.jpg";
+import image17 from "./assets/atumn/atumn-small.jpg";
+////////////////////////////////medium///////////////////
+import image18 from "./assets/atumn/atumn-medium.jpg";
+import image19 from "./assets/atumn/autmn-medium1.jpg";
+import image20 from "./assets/atumn/atum-medum2.jpg";
+////////////////////////////////large///////////////////
+import image21 from "./assets/atumn/atumn-large1.jpg";
+import image22 from "./assets/atumn/atumn-large2.jpg";
+import image23 from "./assets/atumn/atumn-large3.jpg";
+import image24 from "./assets/atumn/atumn-large4.jpg";
+
+const Dataautmn ={
+    autcloth :[
+        {
+            id: '15',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image15,
+    
+          },
+          {
+            id: '16',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image16,
+    
+          },
+          {
+            id: '17',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image17,
+    
+            
+          },
+          {
+            id: '18',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image18,
+    
+            
+          },
+          {
+            id: '19',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image19,
+       
+          },
+          {
+            id: '20',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image20, 
+          },
+          {
+            id: '21',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image21, 
+          },
+          {
+            id: '22',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image22, 
+          },
+          {
+            id: '23',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image23, 
+          },
+          {
+            id: '24',
+            name: 'autmn cloths',
+            price: 1000,
+            image: image24, 
+          },
+    ],
+};
+export default Dataautmn;
---
+++ src/datawinter.js
@@ -0,0 +1,79 @@
+////////////////////////////////small///////////////////
+import image25 from "./assets/winter/winter-small1.jpg";
+import image26 from "./assets/winter/winter-small2.jpg";
+import image27 from "./assets/winter/winter-small3.jpg";
+import image28 from "./assets/winter/winter-small4.jpg";
+////////////////////////////////medium///////////////////
+import image29 from "./assets/winter/winter-medium1.jpg";
+import image30 from "./assets/winter/winter-medium2.jpg";
+import image31 from "./assets/winter/winter-medium3.jpg";
+import image32 from "./assets/winter/winter-medium4.jpg";
+////////////////////////////////large///////////////////
+import image33 from "./assets/winter/winter-large1.jpg";
+import image34 from "./assets/winter/winter-large2.jpg";
+
+const Datawinter ={
+    wincloth:[
+        {
+            id: '25',
+            name: 'winter cloths',
+            price: 1000,
+            image: image25, 
+          },
+          {
+            id: '26',
+            name: 'winter cloths',
+            price: 1000,
+            image: image26, 
+          },
+          {
+            id: '27',
+            name: 'winter cloths',
+            price: 1000,
+            image: image27, 
+          },
+          {
+            id: '28',
+            name: 'winter cloths',
+            price: 1000,
+            image: image28, 
+          },
+          {
+            id: '29',
+            name: 'winter cloths',
+            price: 1000,
+            image: image29, 
+          },
+          {
+            id: '30',
+            name: 'winter cloths',
+            price: 1000,
+            image: image30, 
+          },
+          {
+            id: '31',
+            name: 'winter cloths',
+            price: 1000,
+            image: image31, 
+          },
+          {
+            id: '32',
+            name: 'winter cloths',
+            price: 1000,
+            image: image32, 
+          },
+          {
+            id: '33',
+            name: 'winter cloths',
+            price: 1000,
+            image: image33, 
+          },
+          {
+            id: '34',
+            name: 'winter cloths',
+            price: 1000,
+            image: image34, 
+          },
+    ],
+};
+export default Datawinter;
