76777b1966fdd4f53a6f3cdb25374ee37d97d165
Soroush-Mostafavi <soroush.mr1994@gmail.com>
2022-04-30 13:28:55 +0430

    build data in three part / part one

---
+++ src/datasummer.js
@@ -0,0 +1,112 @@
+///////////////////////////////summer///////////////////
+import image1 from "./assets/summer/summer-dress1.png";
+import image2 from "./assets/summer/summer_dresses2.jpg";
+import image3 from "./assets/summer/summer_dress3.jpg";
+import image4 from "./assets/summer/summer_dresses4.jpg";
+import image5 from "./assets/summer/summer_dress5.jpg";
+import image6 from "./assets/summer/summer_dress6.jpg";
+////////////////////////////////small///////////////////
+import image7 from "./assets/summer/summer-medium.jpg";
+import image8 from "./assets/summer/sumer-medium2.jpg";
+import image9 from "./assets/summer/summer-medum3.jpg";
+import image10 from "./assets/summer/summer-medium4.jpg";
+////////////////////////////////medium///////////////////
+
+import image11 from "./assets/summer/summer-large1.jpg";
+import image12 from "./assets/summer/summer-large2.jpg";
+import image13 from "./assets/summer/summer-large3.jpg";
+import image14 from "./assets/summer/sumer-large4.jpg";
+
+
+
+const Datasummer = {
+    Suncloth: [
+      {
+        id: '1',
+        name: 'summer cloths',
+        price: 1400,
+        image: image1,
+      },
+      {
+        id: '2',
+        name: 'summer cloths',
+        price: 2400,
+        image: image2,
+      },
+      {
+        id: '3',
+        name: 'summer cloths',
+        price: 1000,
+        image: image3,
+      },
+      {
+        id: '4',
+        name: 'summer cloths',
+        price: 1000,
+        image: image4,
+      },
+      {
+        id: '5',
+        name: 'summer cloths',
+        price: 1000,
+        image: image5,
+      },
+      {
+        id: '6',
+        name: 'summer cloths',
+        price: 1000,
+        image: image6,
+      },
+      {
+        id: '7',
+        name: 'summer cloths',
+        price: 1000,
+        image: image7,
+      },
+      {
+        id: '8',
+        name: 'summer cloths',
+        price: 1000,
+        image: image8,
+      },
+      {
+        id: '9',
+        name: 'summer cloths',
+        price: 1000,
+        image: image9,
+      },
+      {
+        id: '10',
+        name: 'summer cloths',
+        price: 1000,
+        image: image10,
+      },
+      {
+        id: '11',
+        name: 'summer cloths',
+        price: 1000,
+        image: image11,
+      },
+      {
+        id: '12',
+        name: 'summer cloths',
+        price: 1000,
+        image: image12,
+      },
+      {
+        id: '13',
+        name: 'summer cloths',
+        price: 1000,
+        image: image13,
+
+      },
+      {
+        id: '14',
+        name: 'summer cloths',
+        price: 1000,
+        image: image14,
+
+      },
+    ],
+};
+export default Datasummer;
